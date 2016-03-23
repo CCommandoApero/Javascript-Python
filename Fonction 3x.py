3c6b31b520da638f774d5005be24d0f7f14a809b
Christophe Chanton <c.christ@neuf.fr>
2016-03-23 08:01:44 +0100

    Update Fonction 3x.py

--- Fonction 3x.py
+++ Fonction 3x.py
@@ -1,5 +1,5 @@
 from matplotlib.pylab import *;
-x = array([0,1,2,3,4,5,6,7,8,9,10])
-y = array([0,3,6,9,12,15,18,21,24,27,30])
-plot(x,y);
-show()
+x = array([0,1,2,3,4,5,6,7,8,9,10]) # X prend la valeur
+y = array([0,3,6,9,12,15,18,21,24,27,30]) # Y prend la valeur
+plot(x,y); # 
+show() # Afficher
